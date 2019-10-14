ca4ef8fccb988e4827d1688b7068bb07be9a9b5a
Jaqueline <53265206+JaqFR@users.noreply.github.com>
2019-10-14 18:32:23 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public int About(int? nr)
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return 1 + (nr.HasValue ? nr.Value : 0);//tem que ter mesmo tipo
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
